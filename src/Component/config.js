d7f5ca8c3dd36c776be0a3748e4ecaba7fffd1ad
Istakhar Ahmad <istakhara21@gmail.com>
2024-08-07 21:04:04 +0530

    format the file structure

---
+++ src/Component/config.js
@@ -0,0 +1,427 @@
+export const CDN_IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const restaurants = [
+    {
+      info: {
+        id: "4923",
+        name: "Anna Ka Dosa",
+        cloudinaryImageId: "cg93igmahfav7mtfpxie",
+        locality: "Deshbandhu College Road",
+        areaName: "Kalkaji",
+        costForTwo: "₹250 for two",
+        cuisines: ["South Indian", "Indian"],
+        avgRating: 3.9,
+        veg: true,
+        feeDetails: {
+          restaurantId: "4923",
+          fees: [
+            {
+              name: "BASE_DISTANCE",
+              fee: 3000,
+            },
+            {
+              name: "BASE_TIME",
+            },
+            {
+              name: "ANCILLARY_SURGE_FEE",
+            },
+          ],
+          totalFee: 3000,
+        },
+        parentId: "5062",
+        avgRatingString: "3.9",
+        totalRatingsString: "10K+",
+        promoted: true,
+        adTrackingId:
+          "cid=7689457~p=1~eid=00000189-aeec-76fa-0154-33ba003e014d~srvts=1690856879866~45826",
+        sla: {
+          deliveryTime: 17,
+          lastMileTravel: 0.4,
+          serviceability: "SERVICEABLE",
+          slaString: "17 mins",
+          lastMileTravelString: "0.4 km",
+          iconType: "ICON_TYPE_EMPTY",
+        },
+        availability: {
+          nextCloseTime: "2023-08-08 00:00:00",
+          opened: true,
+        },
+        badges: {},
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            textExtendedBadges: {},
+            textBased: {},
+            imageBased: {},
+          },
+        },
+        aggregatedDiscountInfoV3: {
+          header: "₹125 OFF",
+          subHeader: "ABOVE ₹249",
+          discountTag: "FLAT DEAL",
+        },
+        orderabilityCommunication: {
+          title: {},
+          subTitle: {},
+          message: {},
+          customIcon: {},
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {},
+            video: {},
+          },
+        },
+        reviewsSummary: {},
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {},
+      },
+    },
+    {
+      info: {
+        id: "420722",
+        name: "Chaayos Chai+Snacks=Relax",
+        cloudinaryImageId: "2beebd3688e63e027ee8a11c1c8a72c8",
+        locality: "H Block",
+        areaName: "Kalkaji",
+        costForTwo: "₹250 for two",
+        cuisines: [
+          "Bakery",
+          "Beverages",
+          "Chaat",
+          "Desserts",
+          "Fast Food",
+          "Home Food",
+          "Italian",
+          "Maharashtrian",
+          "Snacks",
+          "Street Food",
+          "Sweets",
+        ],
+        avgRating: 4.5,
+        feeDetails: {
+          restaurantId: "420722",
+          fees: [
+            {
+              name: "BASE_DISTANCE",
+              fee: 3000,
+            },
+            {
+              name: "BASE_TIME",
+            },
+            {
+              name: "ANCILLARY_SURGE_FEE",
+            },
+          ],
+          totalFee: 3000,
+        },
+        parentId: "281782",
+        avgRatingString: "4.5",
+        totalRatingsString: "1K+",
+        sla: {
+          deliveryTime: 20,
+          lastMileTravel: 0.2,
+          serviceability: "SERVICEABLE",
+          slaString: "20 mins",
+          lastMileTravelString: "0.2 km",
+          iconType: "ICON_TYPE_EMPTY",
+        },
+        availability: {
+          nextCloseTime: "2023-08-01 23:00:00",
+          opened: true,
+        },
+        badges: {},
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            imageBased: {},
+            textExtendedBadges: {},
+            textBased: {},
+          },
+        },
+        aggregatedDiscountInfoV3: {
+          header: "20% OFF",
+          subHeader: "UPTO ₹50",
+        },
+        orderabilityCommunication: {
+          title: {},
+          subTitle: {},
+          message: {},
+          customIcon: {},
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {},
+            video: {},
+          },
+        },
+        reviewsSummary: {},
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {},
+      },
+    },
+    {
+      info: {
+        id: "170910",
+        name: "Chai Point",
+        cloudinaryImageId: "bs7k0xa3261qvh5qhstc",
+        locality: "Block E",
+        areaName: "Kalkaji",
+        costForTwo: "₹150 for two",
+        cuisines: [
+          "Bakery",
+          "Beverages",
+          "Maharashtrian",
+          "Snacks",
+          "Street Food",
+          "South Indian",
+          "Punjabi",
+          "Chaat",
+          "Indian",
+          "American",
+          "North Indian",
+          "Fast Food",
+          "Desserts",
+          "Cafe",
+          "Healthy Food",
+          "Home Food",
+        ],
+        avgRating: 4.2,
+        feeDetails: {
+          restaurantId: "170910",
+          fees: [
+            {
+              name: "BASE_DISTANCE",
+              fee: 3000,
+            },
+            {
+              name: "BASE_TIME",
+            },
+            {
+              name: "ANCILLARY_SURGE_FEE",
+            },
+          ],
+          totalFee: 3000,
+        },
+        parentId: "1607",
+        avgRatingString: "4.2",
+        totalRatingsString: "10K+",
+        sla: {
+          deliveryTime: 30,
+          lastMileTravel: 0.7,
+          serviceability: "SERVICEABLE",
+          slaString: "30 mins",
+          lastMileTravelString: "0.7 km",
+          iconType: "ICON_TYPE_EMPTY",
+        },
+        availability: {
+          nextCloseTime: "2023-08-08 00:00:00",
+          opened: true,
+        },
+        badges: {},
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            textBased: {},
+            imageBased: {},
+            textExtendedBadges: {},
+          },
+        },
+        aggregatedDiscountInfoV3: {
+          header: "60% OFF",
+          subHeader: "UPTO ₹120",
+        },
+        orderabilityCommunication: {
+          title: {},
+          subTitle: {},
+          message: {},
+          customIcon: {},
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {},
+            video: {},
+          },
+        },
+        reviewsSummary: {},
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {},
+      },
+    },
+    {
+      info: {
+        id: "58905",
+        name: "Parantha Express",
+        cloudinaryImageId: "jdjsugeuqaoqxjv6gwx9",
+        locality: "Rajeshwar Arya Marg",
+        areaName: "South Extension 1",
+        costForTwo: "₹150 for two",
+        cuisines: [
+          "North Indian",
+          "Street Food",
+          "Punjabi",
+          "Snacks",
+          "Beverages",
+        ],
+        avgRating: 4.2,
+        feeDetails: {
+          restaurantId: "58905",
+          fees: [
+            {
+              name: "BASE_DISTANCE",
+              fee: 4800,
+            },
+            {
+              name: "BASE_TIME",
+            },
+            {
+              name: "ANCILLARY_SURGE_FEE",
+            },
+          ],
+          totalFee: 4800,
+        },
+        parentId: "10714",
+        avgRatingString: "4.2",
+        totalRatingsString: "10K+",
+        promoted: true,
+        adTrackingId:
+          "cid=7689824~p=2~eid=00000189-aeec-76fa-0154-33bb003e0215~srvts=1690856879866~45826",
+        sla: {
+          deliveryTime: 47,
+          lastMileTravel: 5.8,
+          serviceability: "SERVICEABLE",
+          slaString: "47 mins",
+          lastMileTravelString: "5.8 km",
+          iconType: "ICON_TYPE_EMPTY",
+        },
+        availability: {
+          nextCloseTime: "2023-08-01 23:59:00",
+          opened: true,
+        },
+        badges: {},
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            imageBased: {},
+            textExtendedBadges: {},
+            textBased: {},
+          },
+        },
+        aggregatedDiscountInfoV3: {
+          header: "₹125 OFF",
+          subHeader: "ABOVE ₹249",
+          discountTag: "FLAT DEAL",
+        },
+        orderabilityCommunication: {
+          title: {},
+          subTitle: {},
+          message: {},
+          customIcon: {},
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {},
+            video: {},
+          },
+        },
+        reviewsSummary: {},
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {},
+      },
+    },
+    {
+      info: {
+        id: "22035",
+        name: "FreshMenu",
+        cloudinaryImageId: "6c4de7b481d3614370e5062b2f33e7d6",
+        locality: "Guru Ravidas Marg",
+        areaName: "Kalkaji",
+        costForTwo: "₹250 for two",
+        cuisines: [
+          "Continental",
+          "Chinese",
+          "Oriental",
+          "Asian",
+          "Healthy Food",
+          "Fast Food",
+          "Indian",
+          "Desserts",
+        ],
+        avgRating: 4,
+        feeDetails: {
+          restaurantId: "22035",
+          fees: [
+            {
+              name: "BASE_DISTANCE",
+              fee: 3000,
+            },
+            {
+              name: "BASE_TIME",
+            },
+            {
+              name: "ANCILLARY_SURGE_FEE",
+            },
+          ],
+          totalFee: 3000,
+        },
+        parentId: "398",
+        avgRatingString: "4.0",
+        totalRatingsString: "10K+",
+        sla: {
+          deliveryTime: 32,
+          lastMileTravel: 1.3,
+          serviceability: "SERVICEABLE",
+          slaString: "32 mins",
+          lastMileTravelString: "1.3 km",
+          iconType: "ICON_TYPE_EMPTY",
+        },
+        availability: {
+          nextCloseTime: "2023-08-02 00:00:00",
+          opened: true,
+        },
+        badges: {},
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            textBased: {},
+            imageBased: {},
+            textExtendedBadges: {},
+          },
+        },
+        aggregatedDiscountInfoV3: {
+          header: "EVERY ITEM",
+          subHeader: "@ ₹99",
+        },
+        orderabilityCommunication: {
+          title: {},
+          subTitle: {},
+          message: {},
+          customIcon: {},
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {},
+            video: {},
+          },
+        },
+        reviewsSummary: {},
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {},
+      },
+    },
+  ];
