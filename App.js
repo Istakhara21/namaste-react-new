8f85613c8d6312e61a10321d39041ccbb08b72ea
Istakhar Ahmad <istakhara21@gmail.com>
2024-08-07 20:35:46 +0530

    basic of JS & DOM

---
+++ App.js
@@ -0,0 +1,7 @@
+console.log("heelo hjds")
+
+const root = document.getElementById("root");
+const element = document.createElement("h1");
+element.innerHTML = "Hello World";
+
+root.appendChild(element)
